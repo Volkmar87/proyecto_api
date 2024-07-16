56da1ecf5052afc84788ae1cd707413125ffd7fe
Volkmar87 <aperezlizarriturri@gmail.com>
2024-07-16 17:55:19 +0200

    Add files via upload

--- api/app_model.py
+++ api/app_model.py
@@ -6,9 +6,10 @@ import pandas as pd
 import sqlite3
 from typing import Optional
 
+import test_api
 
 def init_db():
-    df = pd.read_csv('./data/Advertising.csv')
+    df = pd.read_csv('data/Advertising.csv')
     conn = sqlite3.connect('data/advertising.db')
     df.to_sql('Advertising', conn, if_exists='replace', index=False)
     conn.close()
@@ -26,7 +27,7 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"message": "Bienvenido a mi API del modelo advertising"}
+    return {"message": "Bienvenido a mi API del modelo advertising______________________"}
 
 # 1. Endpoint de predicción
 @app.get("/predict")
@@ -41,14 +42,9 @@ async def predict(tv: float, radio: float, newspaper: float):
 
 
 # 2. Endpoint de ingesta de datos
-@app.post("/v1/ingest")
-async def ingest(request: Request):
-    data = await request.json()
-
-    tv = data.get('tv')
-    radio = data.get('radio')
-    newspaper = data.get('newspaper')
-    sales = data.get('sales')
+@app.post("/ingest")
+async def ingest(tv: float, radio: float, newspaper: float, sales: float):
+   
 
     if tv is None or radio is None or newspaper is None or sales is None:
         raise HTTPException(status_code=400, detail="Faltan datos, los valores de TV, radio, newspaper y sales son necesarios")
@@ -57,7 +53,7 @@ async def ingest(request: Request):
     try:
         conn = sqlite3.connect('data/advertising.db')
         c = conn.cursor()
-        c.execute("INSERT INTO advertising_data (tv, radio, newspaper, sales) VALUES (?, ?, ?, ?)", (tv, radio, newspaper, sales))
+        c.execute("INSERT INTO advertising (tv, radio, newpaper, sales) VALUES (?, ?, ?, ?)", (tv, radio, newspaper, sales))
         conn.commit()
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -68,17 +64,17 @@ async def ingest(request: Request):
 
 
 # 3. Endpoint de reentrenamiento del modelo
-@app.post("/v1/retrain")
-def retrain():
+@app.post("/retrain")
+async def retrain():
     try:
         conn = sqlite3.connect('data/advertising.db')
-        df = pd.read_sql_query("SELECT * FROM advertising_data", conn)
+        df = pd.read_sql_query("SELECT * FROM advertising", conn)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     finally:
         conn.close()
-
-    X = df[['tv', 'radio', 'newspaper']]
+    df = df.replace({"6s9.2": "69.2"})
+    X = df[['TV', 'radio', 'newpaper']]
     y = df['sales']
 
     # Reentrenar el modelo existente
